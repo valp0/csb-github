2485635c9df4dfd04e283f55542d97694a6eb5a8
valp0 <75410408+valp0@users.noreply.github.com>
2021-07-19 00:49:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* Learn React */}
-          This is a hyperlink
+          Learn React
         </a>
       </header>
     </div>
